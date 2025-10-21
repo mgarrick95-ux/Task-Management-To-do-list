fbc8f350a2b6bef1ebe31a1978e61469ca263d86
mgarrick95-ux <mgarrick95@gmail.com>
2025-10-21 16:47:55 -0600

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,2 +0,0 @@
-// placeholder - actual app code from canvas will be inserted by user
-export default function App(){return <div className='p-10 text-center text-lg font-semibold'>Insert full app code here from ChatGPT canvas.</div>}
