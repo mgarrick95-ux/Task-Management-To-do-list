4be77e1d7a5695acb6fcb5c3a64c68f588987d5d
mgarrick95-ux <mgarrick95@gmail.com>
2025-10-21 18:55:56 -0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// For GitHub Pages project sites, relative base prevents asset 404s.
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
