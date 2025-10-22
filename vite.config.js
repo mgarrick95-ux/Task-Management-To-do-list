a747ae5fd3137c75b90b646afc2de7ffb46dc2d2
mgarrick95-ux <mgarrick95@gmail.com>
2025-10-21 19:19:31 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: this must match your repo name exactly
+  // must match your repo name exactly (case-sensitive)
   base: '/Task-Management-To-do-list/',
 })
