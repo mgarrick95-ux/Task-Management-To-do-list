dc42e7c040af776efcfcd779b990bebf02a572fb
mgarrick95-ux <mgarrick95@gmail.com>
2025-10-27 13:14:22 -0600

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.js'
+import './styles.css' // if you have styles.css in the repo root
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
