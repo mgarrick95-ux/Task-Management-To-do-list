aec2a2f150e2bb4bc10beccd50819db2aa287e7d
mgarrick95-ux <mgarrick95@gmail.com>
2025-10-27 13:09:00 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Task-Management-To-do-list/',
+  base: '/Task-Management-To-do-list/', // must match your GitHub repo name exactly
 })
