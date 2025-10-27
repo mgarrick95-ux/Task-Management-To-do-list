24ef84a32d17c473e936ebd5591f32b197da2192
mgarrick95-ux <mgarrick95@gmail.com>
2025-10-27 13:49:11 -0600

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.js'
-import './styles.css' // if you have styles.css in the repo root
-
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
