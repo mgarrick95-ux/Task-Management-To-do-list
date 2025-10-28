02613a82cf0aaa2eea14dbd1f1eb37093269bf1c
mgarrick95-ux <mgarrick95@gmail.com>
2025-10-27 18:35:25 -0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Task-Management-To-do-list/',  // <- your repo name, case-sensitive
-})
